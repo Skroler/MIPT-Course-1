79ce83bbdd5f24aecf462d8d3cbdbbb84223aae1
Skrol <yafarov.vv@phystech.edu>
2020-11-16 01:19:37 +0300

    Bit to byte

--- H.cpp
+++ H.cpp
@@ -8,9 +8,9 @@
 
 const long long One = 1ll;
 
-void sorted(std::vector<long long>& A, long long start, long long end, long long bit);
+void sorted(std::vector<long long>& A, long long start, long long end, long long byte);
 
-void LSD_sort(std::vector<long long> & A, long long start, long long end, long long bit);
+void LSD_sort(std::vector<long long> & A, long long start, long long end, long long byte);
 
 int main()
 {
@@ -29,23 +29,23 @@ int main()
     return 0;
 }
 
-void sorted(std::vector<long long>& A, long long start, long long end, long long bit)
+void sorted(std::vector<long long>& A, long long start, long long end, long long byte)
 {
     long long j = end - 1;
     for (int i = start; i < j; ++i) {
-        if (((A[i] >> bit) & One) == 0) continue;
-        while (j > i && ((A[j] >> bit) & One) == 1) --j;
+        if (((A[i] >> byte) & One) == 0) continue;
+        while (j > i && ((A[j] >> byte) & One) == 1) --j;
         if (j > i) std::swap(A[i], A[j]);
     }
 }
 
-void LSD_sort(std::vector<long long> & A, long long start, long long end, long long bit)
+void LSD_sort(std::vector<long long> & A, long long start, long long end, long long byte)
 {
     if (end - start <= 1) return;
-    if (bit == -1) return;
-    sorted(A, start, end, bit);
+    if (byte == -1) return;
+    sorted(A, start, end, byte);
     long long j = start;
-    while (j < end && ((A[j] >> bit) & One) == 0) ++j;
-    LSD_sort(A, start, j, bit - 1);
-    LSD_sort(A, j, end, bit - 1);
+    while (j < end && ((A[j] >> byte) & One) == 0) ++j;
+    LSD_sort(A, start, j, byte - 1);
+    LSD_sort(A, j, end, byte - 1);
 }
